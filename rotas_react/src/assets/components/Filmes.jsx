4d9e1ca542bbfeb85da83192f3d8b0418e3ee1c3
Gustavo Minguta <gustavo.minguta@gmail.com>
2023-11-01 18:15:54 -0300

    aula segunda

---
+++ rotas_react/src/assets/components/Filmes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Filmes(){
+    return(
+        <>
+                <h1>Os melhores filmes para essa semana</h1>
+            
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/qpIuXGFgEb4?si=HE0E5Spv7tM8nrMD" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+             </>
+    )
+}
---
+++ rotas_react/src/assets/components/Musicas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Musicas(){
+    return(
+
+        <>
+        <h2>Este é o componente de Música</h2>
+
+        <iframe src="https://open.spotify.com/embed/track/0KuDeGUXh5QQ7gSiB6KGPh?utm_source=generator" width="100%" height="352" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </>
+    )
+}
